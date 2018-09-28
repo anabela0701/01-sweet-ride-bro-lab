e7b1457dcc9048fda817bbdba575983d39202493
Anabel Arbeeny <anabela0701@hstat.org>
2018-09-28 16:35:27 +0000

    finished with methods thatwerent defined. fixing the errors

--- sweet_ride.rb
+++ sweet_ride.rb
@@ -1,1 +1,20 @@
 #code your answers here
+def check_it_out
+     "Check out my sweet new ride, dude!"
+end 
+
+def no_big_deal(carname)
+     "No big deal, it's a #{carname}"
+end 
+
+def stolen 
+    "dude, where's my car?"
+end 
+
+def reporting_theft(carname)
+    "OFFICER, SOMEONE'S STOLEN MY #{carname.upcase}"
+end 
+
+def how_long_until_i_can_get_another_car(cost_of_car, hourly_wage)
+    cost_of_car / hourly_wage
+end 
